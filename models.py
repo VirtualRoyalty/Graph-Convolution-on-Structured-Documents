f0a845f9b7ce973652e2ef11a0592f9cbebd1faf
Vadik Alperovich <vadikalperovich@mail.ru>
2020-10-27 23:49:46 +0300

    fix scope names

--- models.py
+++ models.py
@@ -12,8 +12,8 @@ tensorflow.enable_eager_execution()
 
 
 # ================== input ====================================================
-adjacency_input = keras.Input(shape=(50,50), batch_size=1, name='Adjacency Input')
-node_input = keras.Input(shape=(50, 5), batch_size=1, name='Node Input')
+adjacency_input = keras.Input(shape=(50,50), batch_size=1, name='Adjacency_Input')
+node_input = keras.Input(shape=(50, 5), batch_size=1, name='Node_Input')
 
 # ================== preprocessing ============================================
 node_embeddings = layers.Dense(units=50, name='Embedding')(node_input)
@@ -28,15 +28,15 @@ adj_list_1.append(node_embeddings)
 # AL
 adj_0_1, adj_1_1, adj_2_1 = Adjacency(n_features=50, max_nodes=50)(adj_list_1)
 
-# GNN 1                                                          
-gnn_1_1 = GNN(n_features=50, n_nodes=50)(inputs = [node_embeddings, adj_0_1, 
+# GNN 1
+gnn_1_1 = GNN(n_features=50, n_nodes=50)(inputs = [node_embeddings, adj_0_1,
                                                     adj_1_1, adj_2_1])
 # BN1 + ReLU
 bn_1_1 = layers.BatchNormalization(name='BN_1.1')(gnn_1_1)
 relu_1 = layers.ReLU(name='ReLU_1')(bn_1_1)
 
 # GNN 2
-gnn_2_1 = GNN(n_features=50, n_nodes=50)(inputs = [relu_1, adj_0_1, adj_1_1, 
+gnn_2_1 = GNN(n_features=50, n_nodes=50)(inputs = [relu_1, adj_0_1, adj_1_1,
                                                     adj_2_1])
 
 # BN2
@@ -53,10 +53,10 @@ adj_list_2 = [adj_tensor for adj_tensor in adj_list]
 adj_list_2.append(add_1)
 
 # AL
-adj_0_2, adj_1_2, adj_2_2 = Adjacency(n_features=50, max_nodes=50)(adj_list_2) 
-                                                            
-# GNN 1                                                          
-gnn_1_2 = GNN(n_features=50, n_nodes=50)(inputs = [add_1, adj_0_2, adj_1_2, 
+adj_0_2, adj_1_2, adj_2_2 = Adjacency(n_features=50, max_nodes=50)(adj_list_2)
+
+# GNN 1
+gnn_1_2 = GNN(n_features=50, n_nodes=50)(inputs = [add_1, adj_0_2, adj_1_2,
                                                             adj_2_2])
 
 # BN1 + ReLU
@@ -64,7 +64,7 @@ bn_1_2 = layers.BatchNormalization(name='BN_2.1')(gnn_1_2)
 relu_2 = layers.ReLU(name='ReLU_2')(bn_1_2)
 
 # GNN 2
-gnn_2_2 = GNN(n_features=50, n_nodes=50)(inputs = [relu_2, adj_0_2, 
+gnn_2_2 = GNN(n_features=50, n_nodes=50)(inputs = [relu_2, adj_0_2,
                                                     adj_1_2, adj_2_2])
 
 # BN2
@@ -81,17 +81,17 @@ adj_list_3 = [adj_tensor for adj_tensor in adj_list]
 adj_list_3.append(add_2)
 
 # AL
-adj_0_3, adj_1_3, adj_2_3 = Adjacency(n_features=50, max_nodes=50)(adj_list_3)                                                            
+adj_0_3, adj_1_3, adj_2_3 = Adjacency(n_features=50, max_nodes=50)(adj_list_3)
 
-# GNN 1                                                          
-gnn_1_3 = GNN(n_features=50, n_nodes=50)(inputs = [add_2, adj_0_3, 
+# GNN 1
+gnn_1_3 = GNN(n_features=50, n_nodes=50)(inputs = [add_2, adj_0_3,
                                                     adj_1_3, adj_2_3])
 # BN1 + ReLU
 bn_1_3 = layers.BatchNormalization(name='BN_3.1')(gnn_1_3)
 relu_3 = layers.ReLU(name='ReLU_3')(bn_1_3)
 
 # GNN 2
-gnn_2_3 = GNN(n_features=50, n_nodes=50)(inputs = [relu_3, adj_0_3, 
+gnn_2_3 = GNN(n_features=50, n_nodes=50)(inputs = [relu_3, adj_0_3,
                                                     adj_1_3, adj_2_3])
 
 # BN2
@@ -115,14 +115,14 @@ node_labels = layers.Dense(14, activation='sigmoid', name='Classifier')(add_3)
 
 opt = SGD(lr = 0.001, momentum=0.9)
 
-model = keras.Model(inputs=[adjacency_input, node_input], 
+model = keras.Model(inputs=[adjacency_input, node_input],
                     outputs=[node_labels])
 
 
 # plot_model(model, './gcn_model.png', show_shapes=True)
 
-# model.compile(optimizer=opt, 
-#             loss='categorical_crossentropy', 
+# model.compile(optimizer=opt,
+#             loss='categorical_crossentropy',
 #             metrics=['accuracy'])
 
 # import keras
