a8b27ce59317e1e5b80b64f7be98d300d97dbaf7
Vadik Alperovich <vadikalperovich@mail.ru>
2020-10-27 23:37:59 +0300

    fix + data

--- grapher.py
+++ grapher.py
@@ -8,7 +8,7 @@ import os
 import networkx as nx
 
 
-class ObjectTree:	
+class ObjectTree:
 	'''
 		Description:
 		-----------
@@ -16,7 +16,7 @@ class ObjectTree:
 			the graph structure:
 				{src_id: [dest_id1, dest_id2, ..]}
 			and return the list of text entities in the input document
-		
+
 		Example use:
 		-----------
 			>> connector = ObjectTree(label_column='label')
@@ -29,17 +29,17 @@ class ObjectTree:
 		self.df = None
 		self.img = None
 		self.count = 0
-	
+
 	def read(self, object_map, image):
-		
+
 		'''
-			Function to ensure the data is in correct format and saves the 
+			Function to ensure the data is in correct format and saves the
 			dataframe and image as class properties
 
 			Args:
 				object_map: pd.DataFrame, having coordinates of bounding boxes,
 										  text object and label
-				
+
 				image: np.array, black and white cv2 image
 
 			Returns:
@@ -64,17 +64,17 @@ class ObjectTree:
 		assert image.ndim == 2, 'Check if the read image is greyscale.'
 
 		# drop unneeded columns
-		required_cols = {'xmin', 'xmax', 'ymin', 'ymax', 'Object', 
+		required_cols = {'xmin', 'xmax', 'ymin', 'ymax', 'Object',
 							self.label_column}
 		un_required_cols = set(object_map.columns) - required_cols
 		object_map.drop(columns=un_required_cols, inplace=True)
-		
+
 		self.df = object_map
 		self.img = image
-		return 
-			 
+		return
+
 	def connect(self, plot=False, export_df=False):
-		
+
 		'''
 			This method implements the logic to generate a graph based on
 			visibility. If a horizontal/vertical line can be drawn from one
@@ -86,9 +86,9 @@ class ObjectTree:
 					the graph is plotted in at path ./grapher_outputs/plots
 
 				export_df (default=False):
-					bool, whether to export the dataframe containing graph 
+					bool, whether to export the dataframe containing graph
 					information;
-					the dataframe is exported as csv to path 
+					the dataframe is exported as csv to path
 					./grapher_outputs/connections
 		'''
 		df, img = self.df, self.img
@@ -99,25 +99,25 @@ class ObjectTree:
 				return
 		except:
 			return
-		
+
 		# initialize empty df to store plotting coordinates
 		df_plot = pd.DataFrame()
-		
+
 		# initialize empty lists to store coordinates and distances
 		# ================== vertical======================================== #
 		distances, nearest_dest_ids_vert = [], []
-		
+
 		x_src_coords_vert, y_src_coords_vert, x_dest_coords_vert, \
 		y_dest_coords_vert = [], [], [], []
 
 		# ======================= horizontal ================================ #
 		lengths, nearest_dest_ids_hori = [], []
-		
+
 		x_src_coords_hori, y_src_coords_hori, x_dest_coords_hori, \
 		y_dest_coords_hori = [], [], [], []
 
 		for src_idx, src_row in df.iterrows():
-			
+
 			# ================= vertical ======================= #
 			src_range_x = (src_row['xmin'], src_row['xmax'])
 			src_center_y = (src_row['ymin'] + src_row['ymax'])/2
@@ -127,7 +127,7 @@ class ObjectTree:
 			# ================= horizontal ===================== #
 			src_range_y = (src_row['ymin'], src_row['ymax'])
 			src_center_x = (src_row['xmin'] + src_row['xmax'])/2
-			
+
 			dest_attr_hori = []
 
 			################ iterate over destination objects #################
@@ -138,41 +138,41 @@ class ObjectTree:
 					# ==================== vertical ==========================#
 					dest_range_x = (dest_row['xmin'], dest_row['xmax'])
 					dest_center_y = (dest_row['ymin'] + dest_row['ymax'])/2
-					
+
 					height = dest_center_y - src_center_y
 
-					# consider only the cases where destination object lies 
+					# consider only the cases where destination object lies
 					# below source
 					if dest_center_y > src_center_y:
-						# check if horizontal range of dest lies within range 
+						# check if horizontal range of dest lies within range
 						# of source
 
 						# case 1
 						if dest_range_x[0] <= src_range_x[0] and \
 							dest_range_x[1] >= src_range_x[1]:
-							
+
 							x_common = (src_range_x[0] + src_range_x[1])/2
-							
+
 							line_src = (x_common , src_center_y)
 							line_dest = (x_common, dest_center_y)
 
 							attributes = (dest_idx, line_src, line_dest, height)
 							dest_attr_vert.append(attributes)
-							
+
 							is_beneath = True
 
 						# case 2
 						elif dest_range_x[0] >= src_range_x[0] and \
 							dest_range_x[1] <= src_range_x[1]:
-							
+
 							x_common = (dest_range_x[0] + dest_range_x[1])/2
-							
+
 							line_src = (x_common, src_center_y)
 							line_dest = (x_common, dest_center_y)
-							
+
 							attributes = (dest_idx, line_src, line_dest, height)
 							dest_attr_vert.append(attributes)
-							
+
 							is_beneath = True
 
 						# case 3
@@ -194,9 +194,9 @@ class ObjectTree:
 						elif dest_range_x[0] <= src_range_x[1] and \
 							dest_range_x[1] >= src_range_x[1] and \
 								dest_range_x[0] > src_range_x[0]:
-							
+
 							x_common = (dest_range_x[0] + src_range_x[1])/2
-							
+
 							line_src = (x_common , src_center_y)
 							line_dest = (x_common, dest_center_y)
 
@@ -204,34 +204,34 @@ class ObjectTree:
 							dest_attr_vert.append(attributes)
 
 							is_beneath = True
-			
+
 				if not is_beneath:
 					# ======================= horizontal ==================== #
 						dest_range_y = (dest_row['ymin'], dest_row['ymax'])
 						# get center of destination NOTE: not used
 						dest_center_x = (dest_row['xmin'] + dest_row['xmax'])/2
-						
+
 						# get length from destination center to source center
 						if dest_center_x > src_center_x:
 							length = dest_center_x - src_center_x
 						else:
 							length = 0
-						
-						# consider only the cases where the destination object 
+
+						# consider only the cases where the destination object
 						# lies to the right of source
 						if dest_center_x > src_center_x:
-							#check if vertical range of dest lies within range 
+							#check if vertical range of dest lies within range
 							# of source
-							
+
 							# case 1
 							if dest_range_y[0] >= src_range_y[0] and \
 								dest_range_y[1] <= src_range_y[1]:
-								
+
 								y_common = (dest_range_y[0] + dest_range_y[1])/2
 
 								line_src = (src_center_x, y_common)
 								line_dest = (dest_center_x, y_common)
-								
+
 								attributes = (dest_idx, line_src, line_dest, length)
 								dest_attr_hori.append(attributes)
 
@@ -239,7 +239,7 @@ class ObjectTree:
 							if dest_range_y[0] <= src_range_y[0] and \
 								dest_range_y[1] <= src_range_y[1] and \
 									dest_range_y[1] > src_range_y[0]:
-								
+
 								y_common = (src_range_y[0] + dest_range_y[1])/2
 
 								line_src = (src_center_x, y_common)
@@ -273,12 +273,12 @@ class ObjectTree:
 								attributes = (dest_idx, line_src, line_dest, length)
 								dest_attr_hori.append(attributes)
 
-			# sort list of destination attributes by height/length at position 
+			# sort list of destination attributes by height/length at position
 			# 3 in tuple
 			dest_attr_vert_sorted = sorted(dest_attr_vert, key = lambda x: x[3])
 			dest_attr_hori_sorted = sorted(dest_attr_hori, key = lambda x: x[3])
-			
-			# append the index and source and destination coords to draw line 
+
+			# append the index and source and destination coords to draw line
 			# ==================== vertical ================================= #
 			if len(dest_attr_vert_sorted) == 0:
 				nearest_dest_ids_vert.append(-1)
@@ -302,8 +302,8 @@ class ObjectTree:
 				y_src_coords_hori.append(-1)
 				x_dest_coords_hori.append(-1)
 				y_dest_coords_hori.append(-1)
-				lengths.append(0)			
-			
+				lengths.append(0)
+
 			else:
 			# try and except for the cases where there are vertical connections
 			# still to be made but all horizontal connections are accounted for
@@ -311,7 +311,7 @@ class ObjectTree:
 					nearest_dest_ids_hori.append(dest_attr_hori_sorted[0][0])
 				except:
 					nearest_dest_ids_hori.append(-1)
-				
+
 				try:
 					x_src_coords_hori.append(dest_attr_hori_sorted[0][1][0])
 				except:
@@ -335,15 +335,16 @@ class ObjectTree:
 				try:
 					lengths.append(dest_attr_hori_sorted[0][3])
 				except:
-					lengths.append(0)			
+					lengths.append(0)
 
 		# ==================== vertical ===================================== #
 		# create df for plotting lines
+		nearest_dest_ids_vert = [ x if x >= 0 else len(df)+x  for x in nearest_dest_ids_vert]
 		df['below_object'] = df.loc[nearest_dest_ids_vert, 'Object'].values
 
 		# add distances column
 		df['below_dist'] = distances
-		
+
 		# add column containing index of destination object
 		df['below_obj_index'] = nearest_dest_ids_vert
 
@@ -352,11 +353,12 @@ class ObjectTree:
 		df_plot['y_src_vert'] = y_src_coords_vert
 		df_plot['x_dest_vert'] = x_dest_coords_vert
 		df_plot['y_dest_vert'] = y_dest_coords_vert
-		
+
 		# df.fillna('NULL', inplace = True)
 
 		# ==================== horizontal =================================== #
 		# create df for plotting lines
+		nearest_dest_ids_hori = [ x if x >= 0 else len(df)+x  for x in nearest_dest_ids_hori]
 		df['side_object'] = df.loc[nearest_dest_ids_hori, 'Object'].values
 
 		# add lengths column
@@ -372,27 +374,27 @@ class ObjectTree:
 		df_plot['y_dest_hori'] = y_dest_coords_hori
 
 		########################## concat df and df_plot ######################
-		
+
 		df_merged = pd.concat([df, df_plot], axis=1)
-		
-		# if an object has more than one parent above it, only the connection 
-		# with the smallest distance is retained and the other distances are 
-		# replaced by '-1' to get such objects, group by 'below_object' column 
+
+		# if an object has more than one parent above it, only the connection
+		# with the smallest distance is retained and the other distances are
+		# replaced by '-1' to get such objects, group by 'below_object' column
 		# and use minimum of 'below_dist'
-		
+
 		# ======================= vertical ================================== #
 		groups_vert = df_merged.groupby('below_obj_index')['below_dist'].min()
-		# groups.index gives a list of the below_object text and groups.values 
+		# groups.index gives a list of the below_object text and groups.values
 		# gives the corresponding minimum distance
 		groups_dict_vert = dict(zip(groups_vert.index, groups_vert.values))
 
 		# ======================= horizontal ================================ #
 		groups_hori = df_merged.groupby('side_obj_index')['side_length'].min()
-		# groups.index gives a list of the below_object text and groups.values 
+		# groups.index gives a list of the below_object text and groups.values
 		# gives the corresponding minimum distance
 		groups_dict_hori = dict(zip(groups_hori.index, groups_hori.values))
-		
-			
+
+
 		revised_distances_vert = []
 		revised_distances_hori = []
 
@@ -400,7 +402,7 @@ class ObjectTree:
 																[], [], [], []
 		rev_x_src_hori, rev_y_src_hori, rev_x_dest_hori, rev_y_dest_hori = \
 																[], [], [], []
-		
+
 		for idx, row in df_merged.iterrows():
 			below_idx = row['below_obj_index']
 			side_idx = row['side_obj_index']
@@ -449,37 +451,37 @@ class ObjectTree:
 		df_merged['y_src_hori'] = rev_y_src_hori
 		df_merged['x_dest_hori'] = rev_x_dest_hori
 		df_merged['y_dest_hori'] = rev_y_dest_hori
-		
-		
-		# plot image if plot==True 
+
+
+		# plot image if plot==True
 		if plot == True:
-		
+
 			# make folder to store output
 			if not os.path.exists('grapher_outputs'):
-				os.makedirs('grapher_outputs')	
-			
+				os.makedirs('grapher_outputs')
+
 			# subdirectory to store plots
 			if not os.path.exists('./grapher_outputs/plots'):
 				os.makedirs('./grapher_outputs/plots')
-			
+
 			# check if image exists in folder
-			try: 
+			try:
 				if len(img) == None:
 					pass
-			except: 
+			except:
 				pass
-			
+
 			# plot if image exists
 			else:
 				for idx, row in df_merged.iterrows():
-					cv2.line(img, 
-							(int(row['x_src_vert']), int(row['y_src_vert'])), 
-							(int(row['x_dest_vert']), int(row['y_dest_vert'])), 
+					cv2.line(img,
+							(int(row['x_src_vert']), int(row['y_src_vert'])),
+							(int(row['x_dest_vert']), int(row['y_dest_vert'])),
 							(0,0,255), 2)
-					
-					cv2.line(img, 
-							(int(row['x_src_hori']), int(row['y_src_hori'])), 
-							(int(row['x_dest_hori']), int(row['y_dest_hori'])), 
+
+					cv2.line(img,
+							(int(row['x_src_hori']), int(row['y_src_hori'])),
+							(int(row['x_dest_hori']), int(row['y_dest_hori'])),
 							(0,0,255), 2)
 
 				# write image in same folder
@@ -490,11 +492,11 @@ class ObjectTree:
 
 		# export dataframe with destination objects to csv in same folder
 		if export_df == True:
-			
+
 			# make folder to store output
 			if not os.path.exists('grapher_outputs'):
-				os.makedirs('grapher_outputs')			
-			
+				os.makedirs('grapher_outputs')
+
 			# subdirectory to store plots
 			if not os.path.exists('./grapher_outputs/connections'):
 				os.makedirs('./grapher_outputs/connections')
@@ -502,7 +504,7 @@ class ObjectTree:
 			CSV_PATH = \
 				'./grapher_outputs/connections/' + 'connections_' + str(self.count) + '.csv'
 			df.to_csv(CSV_PATH, index = None)
-			 
+
 		# convert dataframe to dict:
 		# {src_id: dest_1, dest_2, ..}
 
@@ -512,8 +514,8 @@ class ObjectTree:
 				graph_dict[src_id] = [row['below_obj_index']]
 			if row['side_obj_index'] != -1:
 				graph_dict[src_id] = [row['side_obj_index']]
-			
-		
+
+
 		return graph_dict, df['Object'].tolist()
 
 
@@ -530,12 +532,12 @@ class Graph:
 	# 		Function to make networkx graph
 
 	# 		Args:
-	# 			graph_dict: dict of lists, 
+	# 			graph_dict: dict of lists,
 	# 						{src_id: [dest_id]}
 
-				
+
 	# 		Returns:
-	# 			G: 
+	# 			G:
 	# 				Padded adjacency matrix of size (max_nodes, max_nodes)
 
 	# 			feats:
@@ -546,18 +548,18 @@ class Graph:
 
 	# 	return G
 
-	def _get_text_features(self, data): 
-    
+	def _get_text_features(self, data):
+
 		'''
 			Args:
 				str, input data
-				
-			Returns: 
+
+			Returns:
 				np.array, shape=(22,);
 				an array of the text converted to features
-				
+
 		'''
-		
+
 		assert type(data) == str, f'Expected type {str}. Received {type(data)}.'
 
 		n_upper = 0
@@ -568,45 +570,45 @@ class Graph:
 		n_numeric = 0
 		n_special = 0
 		number = 0
-		special_chars = {'&': 0, '@': 1, '#': 2, '(': 3, ')': 4, '-': 5, '+': 6, 
-						'=': 7, '*': 8, '%': 9, '.':10, ',': 11, '\\': 12,'/': 13, 
+		special_chars = {'&': 0, '@': 1, '#': 2, '(': 3, ')': 4, '-': 5, '+': 6,
+						'=': 7, '*': 8, '%': 9, '.':10, ',': 11, '\\': 12,'/': 13,
 						'|': 14, ':': 15}
-		
-		special_chars_arr = np.zeros(shape=len(special_chars))    
-		
+
+		special_chars_arr = np.zeros(shape=len(special_chars))
+
 		# character wise
-		for char in data: 
-			
-			# for lower letters 
-			if char.islower(): 
+		for char in data:
+
+			# for lower letters
+			if char.islower():
 				n_lower += 1
-	
-			# for upper letters 
-			if char.isupper(): 
+
+			# for upper letters
+			if char.isupper():
 				n_upper += 1
-			
+
 			# for white spaces
 			if char.isspace():
 				n_spaces += 1
-			
+
 			# for alphabetic chars
 			if char.isalpha():
 				n_alpha += 1
-			
+
 			# for numeric chars
 			if char.isnumeric():
 				n_numeric += 1
-			
+
 			# array for special chars
 			if char in special_chars.keys():
 				char_idx = special_chars[char]
 				# put 1 at index
 				special_chars_arr[char_idx] += 1
-				
+
 		# word wise
 		for word in data.split():
-			
-			# if digit is integer 
+
+			# if digit is integer
 			try:
 				number = int(word)
 				n_digits += 1
@@ -620,49 +622,49 @@ class Graph:
 					n_digits += 1
 				except:
 					pass
-		
+
 		features = []
 		features.append([n_lower, n_upper, n_spaces, n_alpha, n_numeric, n_digits])
 		features = np.array(features)
 		features = np.append(features, np.array(special_chars_arr))
-		
+
 		return features
 
 	def _pad_adj(self, adj):
 		'''
-			This method resizes the input Adjacency matrix to shape 
+			This method resizes the input Adjacency matrix to shape
 			(self.max_nodes, self.max_nodes)
 
-			adj: 
+			adj:
 				2d numpy array
 				adjacency matrix
 		'''
-		
+
 		assert adj.shape[0] == adj.shape[1], f'The input adjacency matrix is \
 			not square and has shape {adj.shape}'
-		
+
 		# get n of nxn matrix
 		n = adj.shape[0]
-		
+
 		if n < self.max_nodes:
 			target = np.zeros(shape=(self.max_nodes, self.max_nodes))
 
 			# fill in the target matrix with the adjacency
 			target[:adj.shape[0], :adj.shape[1]] = adj
-			
+
 		elif n > self.max_nodes:
 			# cut away the excess rows and columns of adj
 			target = adj[:self.max_nodes, :self.max_nodes]
-			
+
 		else:
 			# do nothing
 			target = adj
-			
+
 		return target
-	
+
 	def _pad_text_features(self, feat_arr):
 		'''
-			This method pads the feature matrix to size 
+			This method pads the feature matrix to size
 			(self.max_nodes, feat_arr.shape[1])
 		'''
 		target = np.zeros(shape=(self.max_nodes, feat_arr.shape[1]))
@@ -673,7 +675,7 @@ class Graph:
 		elif self.max_nodes < feat_arr.shape[0]:
 			target = feat_arr[:self.max_nodes, feat_arr.shape[1]]
 
-		else: 
+		else:
 			target = feat_arr
 
 		return target
@@ -686,7 +688,7 @@ class Graph:
 
 			Args:
 				G: networkx graph object
-				
+
 				text_list: list,
 							of text entities:
 							['Tax Invoice', '1/2/2019', ...]
@@ -710,7 +712,7 @@ class Graph:
 
 		return A, X
 
-	
+
 
 
 
@@ -718,18 +720,18 @@ if __name__ == "__main__":
 	print(os.getcwd())
 	df = pd.read_csv('/mnt/data/Workarea/dhaval/visibilty_based_graph/grapher_test/draw_test1/object_map.csv')
 	img = cv2.imread('/mnt/data/Workarea/dhaval/visibilty_based_graph/grapher_test/draw_test1/deskew.jpg', 0)
-	
+
 	tree = ObjectTree()
 	tree.read(df, img)
-	
+
 	graph_dict, text_list = tree.connect(plot=True, export_df=True)
-	
+
 	print(graph_dict)
 	print('\n--------------------------------------------------------------\n')
 
 	graph = Graph()
 	A, X = graph.make_graph_data(graph_dict, text_list)
-	
+
 
 	print(A)
 	print('-----------------------------------------------------------------\n')
